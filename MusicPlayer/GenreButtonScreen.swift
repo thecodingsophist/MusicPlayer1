bde7ceb8d2f8452f83008ff7112fa8cf616a424b
Zhanxi Ni <ni.zhanxi@gmail.com>
2019-08-15 06:36:29 -0700

    Initial Commit

---
+++ MusicPlayer/GenreButtonScreen.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class GenreButtonScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func GenreButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func StopButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func PlayButtonTapped(_ sender: Any) {
+    }
+    
+}
